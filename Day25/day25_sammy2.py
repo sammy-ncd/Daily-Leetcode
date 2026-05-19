78b83a07d808edcadb0c1649d7aa500109715d39
sammy-ncd <jensonsnoww@gmail.com>
2026-05-19 06:04:07 -0700

    another one

---
+++ Day25/day25_sammy2.py
@@ -0,0 +1,42 @@
+"""
+
+Problem:
+https://leetcode.com/problems/robot-return-to-origin/description/?envType=daily-question&envId=2026-05-19
+
+"""
+
+
+
+"""
+
+Approach:
+Check that we have performed both same number of up and down moves as well and left and right moves.
+
+"""
+
+
+def judgeCircle(moves):
+    dirs = {
+        'U' : 0,
+        'D' : 0,
+        'L' : 0,
+        'R' : 0
+    }
+    
+    for move in moves:
+        dirs[move] += 1
+    
+    return dirs['U'] == dirs['D'] and dirs['L'] == dirs['R']
+
+
+"""
+
+Time complexity:
+simply looping through the string ----> Overall: O(n)
+
+
+Space complexity:
+just very small hashmap used its negligable ----> Overall: O(1)
+
+
+"""
