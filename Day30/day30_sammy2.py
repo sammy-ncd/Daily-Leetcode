8a6fd821e7dcac7c1a5b86965c4a740d77dc801f
sammy-ncd <jensonsnoww@gmail.com>
2026-05-24 01:55:12 -0700

    another for fun

---
+++ Day30/day30_sammy2.py
@@ -0,0 +1,45 @@
+"""
+
+Problem:
+https://leetcode.com/problems/invert-binary-tree/description/?envType=problem-list-v2&envId=binary-tree
+
+
+"""
+
+"""
+
+Approach:
+simply replace left subtree with right subtree then recurse down these trees
+
+
+"""
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def invertTree(self, root):
+
+    if not root:
+        return
+
+    root.left, root.right = root.right, root.left
+    self.invertTree(root.left)
+    self.invertTree(root.right)        
+    
+    return root
+
+
+"""
+
+Time complexity:
+we do the swapping for all nodes in the tree so overall ----> O(n)
+
+Space complexity:
+no additional memory used ---> overall O(1)
+
+
+"""
